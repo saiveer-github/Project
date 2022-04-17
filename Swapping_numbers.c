4ae978d39448a8447317b425877ebc72cbdd520b
saiveer-github <103894691+saiveer-github@users.noreply.github.com>
2022-04-17 21:59:55 +0530

    Update Swapping_numbers.c

--- Swapping_numbers.c
+++ Swapping_numbers.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <stdio.h> //Here I declared the standard input output header file i.e preprocessor
 
 int main()
 {
-    int a,b;
+    int a,b;  //Declared two variables that are needed to be swapped
     printf("Enter the values of a and b\n");
-    scanf("%d%d",&a,&b);
+    scanf("%d%d",&a,&b);  //scanning the values of the two variables from the user
     a=a+b;
-    b=a-b;
+    b=a-b;     //performing some mathematical operations required for swapping
     a=a-b;
     printf("After swapping the value of a is %d and the value of b is %d",a,b);
 
