7ff0158c9d1dc931e40a014fab73524c497b1326
saiveer-github <103894691+saiveer-github@users.noreply.github.com>
2022-04-17 22:05:23 +0530

    created a c program for swapping numbers

--- Swapping_numbers.c
+++ Swapping_numbers.c
@@ -4,7 +4,7 @@ int main()
 {
     int a,b;  //Declared two variables that are needed to be swapped
     printf("Enter the values of a and b\n");
-    scanf("%d%d",&a,&b);  //scanning the values of the two variables from the user
+    scanf("%d%d",&a,&b);  //Scanning the values of the two variables from the user
     a=a+b;
     b=a-b;     //performing some mathematical operations required for swapping
     a=a-b;
