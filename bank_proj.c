469ede8f664ecad4d46e0836e902401afaea7737
saiveer-github <103894691+saiveer-github@users.noreply.github.com>
2022-05-01 16:06:02 +0530

    created the project for bank management system

---
+++ bank_proj.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+int i,j;
+int main_exit;
+void main()
+{
+  char pass [10],
+  password [10]=^ prime prime mycap^ prime prime ;
+  int i = 0 ; 
+  printf("\n\n\t Enter the password to og in^ prime prime ) ;
+
+ scanf (mo 6 \%5^ 11 , pass);
+
+if (pass[i] !=13\&\&p [i]!=8) {
+
+printf(""); 
+pass [i] = g*epsilon (); 
+i++
+
+} 
+while(pass [11!=13) ;
+pass [10] =^ prime prime 10^ prime prime ;
+if(strcmp(pass, password (d)=0) 
+printf("\n\n Password Ech^ prime prime ) ; for (i=0,i<=6,1++)
+
+{
+
+}
+
+fordelay(1000); printf(".");
+
+}
+
+system("cls"); menu();
+
+printf("\n\n Wrong Password");
+print("Enter 1 to retry & 0 to exit":
+scanf("%d", &main_exit); 
+if (main_exit t==1 ) {
+
+system("cls"); 
+main();
+
+}
+
+else if (main_exit=0)
+{
+    system("cls");
+    close()
+}
+
+else
+
+{ printf("\n invalid");
+fordelay(1000);
+goto login_try
+
+}
